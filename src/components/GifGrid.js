ea98a6b1dae600d146f8de8fbb2a6b1e1ed309c4
michael <stevenpnd@gmail.com>
2021-08-28 17:33:22 -0500

    Apply design to displayed images

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -25,9 +25,11 @@ export const GifGrid = ({ category }) => {
     return (
         <>
             <h3>{category}</h3>
-            {
-                images.map(img => <GifGridItem key={img.id} {...img} />)
-            }
+            <div className="card-grid">
+                {
+                    images.map(img => <GifGridItem key={img.id} {...img} />)
+                }
+            </div>
         </>
     )
 }
