15a40a6af1ce9212038b1caf465961df5c283265
michael <stevenpnd@gmail.com>
2021-08-29 20:15:48 -0500

    Refactor: Use custom hooks to get images

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,21 +1,15 @@
 import React from 'react'
-import { useState, useEffect } from 'react';
-import { getGifs } from '../helpers/getGifs';
+import { useFetchGifs } from '../hooks/useFetchGifs';
 import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
 
-    const [images, setImages] = useState([]);
-
-    useEffect(() => {
-        getGifs(category).then(setImages)
-    }, [category]);
-
-
+    const { data: images, loading } = useFetchGifs(category);
 
     return (
         <>
             <h3>{category}</h3>
+            {loading && <p>Loading</p>}
             <div className="card-grid">
                 {
                     images.map(img => <GifGridItem key={img.id} {...img} />)
